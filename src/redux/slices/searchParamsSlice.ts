a1786067ec3e46b9973edd0966b1ed7107c8e7cf
eren <e.atef@unityco.io>
2023-04-29 18:27:45 +0300

    QP-210

--- src/redux/slices/searchParamsSlice.ts
+++ src/redux/slices/searchParamsSlice.ts
@@ -1,38 +1,46 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Area, Branch, Category, SearchParams } from '@/types/queries';
-import { branchSlice } from './branchSlice';
-import { RootState } from '../store';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Area, Branch, Category, SearchParams } from "@/types/queries";
+import { RootState } from "../store";
 
 const initialState: SearchParams = {
-  method: undefined,
-  destination: undefined,
-  destination_type: undefined,
+  method: null,
+  destination: null,
+  destination_type: null,
   category_id: null,
 };
 
 export const searchParamsSlice = createSlice({
-  name: 'searchParams',
+  name: "searchParams",
   initialState,
   reducers: {
-    setDestination: (state: typeof initialState, action: PayloadAction<{
-      destination: Branch | Area; method: 'pickup'
-      | 'delivery'
-    }>) => ({
+    setDestination: (
+      state: typeof initialState,
+      action: PayloadAction<{
+        destination: Branch | Area;
+        method: "pickup" | "delivery";
+      }>
+    ) => ({
       ...state,
       method: action.payload.method,
-      destination_type: action.payload.method === 'pickup' ? 'branch' : 'area',
+      destination_type: action.payload.method === "pickup" ? "branch" : "area",
 
-      destination: action.payload.destination
+      destination: action.payload.destination,
     }),
-    setCategory: (state: typeof initialState, action: PayloadAction<number>) => ({
+    setCategory: (
+      state: typeof initialState,
+      action: PayloadAction<number>
+    ) => ({
       ...state,
       category_id: action.payload,
     }),
-  }
+  },
 });
 
 export const { setDestination, setCategory } = searchParamsSlice.actions;
 export const destinationId = (state: RootState) => state.destination.id;
-export const destinationObject = (state: RootState) => (state.method === 'pickup' ? { branch_id: state.destination?.id } : { area_id: state.destination?.id });
+export const destinationObject = (state: RootState) =>
+  state.method === "pickup"
+    ? { branch_id: state.destination?.id }
+    : { area_id: state.destination?.id };
 // branch_id: method !== `pickup` ? branch_id : ``,
 //   area_id: method === `pickup` ? area_id : ``,
