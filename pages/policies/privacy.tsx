9390c45e68cd9ceed7c7c3ef1bdef6a78417e9ca
eren <e.atef@unityco.io>
2023-04-29 10:12:03 +0300

    QP-210

---
+++ pages/policies/privacy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function PrivacyPolicy({}: Props) {
+  return (
+<div>privacy</div>
+  )
+}
---
+++ pages/policies/return.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function ReturnPolicy({}: Props) {
+  return (
+<div>Return</div>
+  )
+}
