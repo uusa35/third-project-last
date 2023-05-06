f0c10f09b4d9a5682c79050285be9323021cb40c
uusa35 <uusa35@gmail.com>
2023-05-06 15:48:59 +0300

    updates

--- pages/order/track/index.tsx
+++ pages/order/track/index.tsx
@@ -8,10 +8,12 @@ import { NextPage } from 'next';
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
+  MapIcon,
   MapPinIcon,
 } from '@heroicons/react/24/outline';
 import { useAppSelector } from '@/redux/hooks';
 import { useTranslation } from 'react-i18next';
+import { SendOutlined } from '@mui/icons-material';
 
 type Props = {
   element: Vendor;
@@ -30,7 +32,7 @@ const OrderTrack: NextPage<Props> = ({ element, url }): React.ReactElement => {
       currentModule="orders"
     >
       <div className="flex flex-1 w-full flex-col justify-center items-start mt-8">
-        <div className="flex flex-1 flex-col w-full border-b-8 border-gray-100 pb-8 px-3">
+        <div className="flex flex-1 flex-col w-full border-b-8 border-gray-100 pb-6 mb-6 px-3">
           <h1 className="text-xl text-extrabold">
             Order Received, We have got your order..
           </h1>
@@ -38,6 +40,7 @@ const OrderTrack: NextPage<Props> = ({ element, url }): React.ReactElement => {
             <p className="text-md text-gray-400 mr-2">Estimated time :</p>{' '}
             <span>2:00-2:30 PM</span>
           </div>
+          {/* order id  */}
           <div className="flex flex-1 w-full flex-row justify-between items-center h-1 my-6">
             <div className="w-1/3 bg-red-600 h-1"></div>
             <div className="w-1/3 bg-gray-200 h-1 mx-1.5"></div>
@@ -48,6 +51,50 @@ const OrderTrack: NextPage<Props> = ({ element, url }): React.ReactElement => {
             <p>#34234324</p>
           </div>
         </div>
+        <div className="flex flex-1 flex-col w-full px-3 border-b-8 border-gray-100 pb-6">
+          <div className="capitlize text-xl mb-4">Pick up from</div>
+          <div className="flex w-full flex-row justify-between items-center ">
+            <div className={`p-1 bg-gray-100 rounded-full`}>
+              <MapPinIcon className="h-5 w-5 text-black" />
+            </div>
+            <div className="flex flex-1 w-full flex-col mx-3">
+              <p className="flex flex-1">Branch Address</p>
+              <p>ManSource, El Geesh St</p>
+            </div>
+            <div className="flex ">
+              <button className="btn bg-gray-100 p-3 flex justify-center items-center rounded-full text-xs">
+                <SendOutlined className="h-3 w-3 text-black mx-1" />
+                Get Direction
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col space-y-4 absolute bottom-0 w-full border-t border-gray-200 p-4">
+        <button
+          className={`flex flex-row w-full justify-center items-center space-x-3 rounded-3xl bg-red-600 p-3 py-4 text-white capitlaize`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+          <p className="text-md text-center">{t('add_order')}</p>
+        </button>
+        <button
+          className={`flex flex-row w-full justify-center items-center space-x-3 rounded-3xl bg-white p-3 py-4 text-red-600 border border-red-600 capitalize`}
+        >
+          <p className="text-md text-center">{t('cancel order')}</p>
+        </button>
       </div>
     </MainContentLayout>
   );
