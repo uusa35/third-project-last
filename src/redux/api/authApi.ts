82d7fd3042dadf0969b65628b66f7e5d1cc4d5db
ES484 <esraasaad478@gmail.com>
2023-05-31 15:32:43 +0200

    auth slice

---
+++ src/redux/api/authApi.ts
@@ -0,0 +1,83 @@
+import { apiSlice } from './index';
+import { AppQueryResult, PhoneCheck, Register, VerifyCode } from '@/types/queries';
+
+export const authApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    checkPhone: builder.mutation<
+      AppQueryResult<PhoneCheck>,
+      {
+        body: {
+            phone: string;
+            phone_country_code: number;
+        };
+        url: string;
+      }
+    >({
+      query: ({ body, url }) => ({
+        url: `phone-check`,
+        method: `POST`,
+        headers: { url },
+        body,
+        validateStatus: (response, result) => result.status,
+      }),
+    }),
+    verifyCode: builder.mutation<
+      AppQueryResult<VerifyCode>,
+      {
+        body: {
+            phone: string;
+            phone_country_code: number;
+            code: string,
+            type: 'register' | 'rest'
+        };
+        url: string;
+      }
+    >({
+      query: ({ body, url }) => ({
+        url: `verify`,
+        method: `POST`,
+        headers: { url },
+        body,
+        validateStatus: (response, result) => result.status,
+      }),
+    }),
+    register: builder.mutation<
+      AppQueryResult<PhoneCheck>,
+      {
+        body: {
+            phone: string;
+            phone_country_code: number;
+        };
+        url: string;
+      }
+    >({
+      query: ({ body, url }) => ({
+        url: `register`,
+        method: `POST`,
+        headers: { url },
+        body,
+        validateStatus: (response, result) => result.status,
+      }),
+    }),
+    login: builder.mutation<
+      AppQueryResult<Register>,
+      {
+        body: {
+            phone: string;
+            phone_country_code: number;
+        };
+        url: string;
+      }
+    >({
+      query: ({ body, url }) => ({
+        url: `login`,
+        method: `POST`,
+        headers: { url },
+        body,
+        validateStatus: (response, result) => result.status,
+      }),
+    }),
+  }),
+});
+
+export const { useCheckPhoneMutation } = authApi;
