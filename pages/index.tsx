164d720290a47391fa091b03f6e0a01b093b46f8
uusa35 <uusa35@gmail.com>
2023-06-18 13:45:33 +0300

    updages

--- pages/index.tsx
+++ pages/index.tsx
@@ -184,7 +184,7 @@ const Home: NextPage<Props> = ({
             <>
               <div className={`py-4`}>
                 {!isEmpty(categories) &&
-                vendorElement?.Data?.template_type === 'basic_category' ? (
+                vendorElement?.Data?.template_type === 'THEME_TWO_CATEGORY_LIST' ? (
                   <>
                     <UpcomingOrders />
                     <div className="px-4">
