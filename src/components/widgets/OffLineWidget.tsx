b8a2cf28d9bd60c21cfb7d54d55808231341980e
ES484 <esraasaad478@gmail.com>
2023-07-02 11:36:53 +0200

    401, 404 pages

--- src/components/widgets/OffLineWidget.tsx
+++ src/components/widgets/OffLineWidget.tsx
@@ -15,50 +15,56 @@ import { useAppSelector } from '@/redux/hooks';
 type Props = {
   message: string;
   img?: string;
+  desc?: string;
+  buttons?: React.ReactNode;
 };
-const OffLineWidget: FC<Props> = ({ message, img = null }): JSX.Element => {
+const OffLineWidget: FC<Props> = ({ message, img = null, desc='', buttons }): JSX.Element => {
   const { t } = useTranslation();
   const color = useAppSelector(themeColor);
   return (
     <Suspense>
-      <div
-        className={`flex w-full flex-col justify-center items-center space-y-10 mt-10 px-4`}
-      >
-        {!isNull(img) ? (
-          <Image
-            className="h-90 w-full rounded-lg shadow-lg"
-            alt="offline"
-            fill={false}
-            width={imageSizes.lg}
-            height={imageSizes.lg}
-            src={img}
-          />
-        ) : (
-          <Image
-            className="h-90 w-90"
-            alt="offline"
-            fill={false}
-            width={imageSizes.xs}
-            height={imageSizes.xs}
-            src={require('@/appImages/offline.webp')}
-          />
-        )}
-        <p
-          className={`text-lg text-center whitespace-wrap break-words w-full`}
-          suppressHydrationWarning={suppressText}
+      <div className='min-h-screen flex items-center'>
+        <div
+          className={`flex w-full flex-col justify-center items-center mt-10 px-4`}
         >
-          {/* {message} */}
-          {t(message)}
-        </p>
-        <Link
-          scroll={true}
-          href={'/'}
-          className={`${submitBtnClass} text-center text-md capitalize`}
-          suppressHydrationWarning={suppressText}
-          style={{ backgroundColor: convertColor(color, 100) }}
-        >
-          {t('back_to_home')}
-        </Link>
+          {!isNull(img) ? (
+            <Image
+              className="rounded-lg"
+              alt="offline"
+              fill={false}
+              width={250}
+              height={250}
+              src={img}
+            />
+          ) : (
+            <Image
+              className="h-90 w-90"
+              alt="offline"
+              fill={false}
+              width={imageSizes.xs}
+              height={imageSizes.xs}
+              src={require('@/appImages/offline.webp')}
+            />
+          )}
+          <p
+            className={`text-lg text-center font-semibold whitespace-wrap break-words w-full pt-5 pb-3`}
+            suppressHydrationWarning={suppressText}
+          >
+            {/* {message} */}
+            {t(message)}
+          </p>
+          <p className='text-[#544A45] text-center lowercase'>{t(desc)}</p>
+          {/* <Link
+            scroll={true}
+            href={'/'}
+            className={`${submitBtnClass} text-center text-md capitalize`}
+            suppressHydrationWarning={suppressText}
+            style={{ backgroundColor: convertColor(color, 100) }}
+          >
+            {t('back_to_home')}
+          </Link> */}
+          {buttons}
+        </div>
       </div>
     </Suspense>
   );
