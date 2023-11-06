d64c000df979e175c012e5cf8923a5edd31cebae
eren <erenyatef.g@gmail.com>
2023-11-06 12:23:07 +0200

    fix promo issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,6 +68,7 @@ const Home: NextPage<Props> = ({
     appSetting: { lastHomeModalShownTime, HomePromoCodeOpen },
     PromoCode: { closedModals },
   } = useAppSelector((state) => state);
+  const [modalData, setModalData] = useState<HomePromoCode[]>([]);
   const dispatch = useAppDispatch();
   const DestinationId = useAppSelector(destinationId);
   const desObject = useAppSelector(destinationHeaderObject);
@@ -158,9 +159,33 @@ const Home: NextPage<Props> = ({
     return exist;
   };
 
-  const handelHomePromoCodeShowTime = () => {
+  // const handelHomePromoCodeShowTime = () => {
+  //   if (isEmpty(closedModals)) {
+  //     // return homePromocodeData?.data;
+  //     setModalData(homePromocodeData?.data);
+  //   } else {
+  //     // remove expired data from state
+  //     // check on each obj from server if it exist in state
+  //     dispatch(removeExpiredPromoCodes());
+
+  //     let validPromoCodes: HomePromoCode[] = [];
+  //     // loop on all promodata
+  //     forEach(homePromocodeData?.data, (item) => {
+  //       if (!checkItemExistInHiddenModals(item.promo_code_id))
+  //         validPromoCodes.push(item);
+  //     });
+
+  //     console.log({ validPromoCodes, closedModals });
+
+  //     // return validPromoCodes;
+  //     setModalData(validPromoCodes);
+  //   }
+  // };
+
+  useEffect(() => {
     if (isEmpty(closedModals)) {
-      return homePromocodeData?.data;
+      // return homePromocodeData?.data;
+      setModalData(homePromocodeData?.data);
     } else {
       // remove expired data from state
       // check on each obj from server if it exist in state
@@ -173,11 +198,12 @@ const Home: NextPage<Props> = ({
           validPromoCodes.push(item);
       });
 
-      console.log({ validPromoCodes, closedModals });
+      console.log({ validPromoCodes, closedModals }, homePromocodeData?.data);
 
-      return validPromoCodes;
+      // return validPromoCodes;
+      setModalData(validPromoCodes);
     }
-  };
+  }, [homePromocodeData]);
 
   return (
     <Suspense>
@@ -267,15 +293,15 @@ const Home: NextPage<Props> = ({
               <CheckoutFixedBtn url={url} />
               {homePromocodeSuccess &&
                 homePromocodeData?.data &&
-                !isEmpty(homePromocodeData?.data) &&(
+                !isEmpty(homePromocodeData?.data) && (
                   <HomeModal
-                    data={handelHomePromoCodeShowTime()}
+                    data={modalData}
                     isOpen={HomePromoCodeOpen}
                     onRequestClose={() => {
                       dispatch(
                         addToHiddenModals({
                           closedDate: new Date().toString(),
-                          id: handelHomePromoCodeShowTime()[0].promo_code_id,
+                          id: modalData[0].promo_code_id,
                         })
                       );
                       dispatch(homePromoCodeOpenModal(false));
